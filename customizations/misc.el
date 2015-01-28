852120a865ce1600c90ce38520350a2a49b077c0
Jens Ostlund <jostlund@gmail.com>
2015-01-28 11:35:46 +0100

    intial commit of emacs settings

---
+++ customizations/misc.el
@@ -0,0 +1,19 @@
+;; enables acute and grave symbols
+;(require 'iso-transl)
+(define-key key-translation-map [dead-grave] (lookup-key key-translation-map "\C-x8`"))
+(define-key key-translation-map [dead-acute] (lookup-key key-translation-map "\C-x8'"))
+(define-key isearch-mode-map [dead-grave] nil)
+(define-key isearch-mode-map [dead-acute] nil)
+
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
