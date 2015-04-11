b1738037f172c0acd2fbe2b2eb68a4d4d7162267
Jens Ostlund <jostlund@gmail.com>
2015-04-11 12:52:12 +0200

    Fixed haskell mode

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -9,55 +9,60 @@
 ;; hoogle (remember to run 'hoogle data' in terminal to populate db)
 ;; structured-haskell-mode
 
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook
+          (lambda () (ghc-init)))
+
 ;; set indentation
 
 ;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ; (add-hook 'haskell-mode-hook 'turn-on-hi2)
 ; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
 
-(add-hook 'haskell-mode-hook 
-          '(lambda ()
-             (setq haskell-indent-mode t)))
+;; (add-hook 'haskell-mode-hook 
+;;           '(lambda ()
+;;              (setq haskell-indent-mode t)))
 
-;; Map import navigation
-(eval-after-load 'haskell-mode
-  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
+;; ;; Map import navigation
+;; (eval-after-load 'haskell-mode
+;;   '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
 
-;; Hasktags
-(let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
-  (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
-  (add-to-list 'exec-path my-cabal-path))
-(custom-set-variables '(haskell-tags-on-save t))
+;; ;; Hasktags
+;; (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+;;   (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+;;   (add-to-list 'exec-path my-cabal-path))
+;; (custom-set-variables '(haskell-tags-on-save t))
 
-;; Haskell-mode key re-mappings
-(custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
-(eval-after-load 'haskell-mode '(progn
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
-(eval-after-load 'haskell-cabal '(progn
-  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
+;; ;; Haskell-mode key re-mappings
+;; (custom-set-variables
+;;   '(haskell-process-suggest-remove-import-lines t)
+;;   '(haskell-process-auto-import-loaded-modules t)
+;;   '(haskell-process-log t))
+;; (eval-after-load 'haskell-mode '(progn
+;;   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+;;   (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+;;   (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+;;   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+;;   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+;;   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+;;   (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+;; (eval-after-load 'haskell-cabal '(progn
+;;   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+;;   (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+;;   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+;;   (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
-;; REPL
-(custom-set-variables '(haskell-process-type 'cabal-repl))
+;; ;; REPL
+;; (custom-set-variables '(haskell-process-type 'cabal-repl))
 
-;; ghc-mod setup (turn off to use haskell-mode's features, which are a bit heavier
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;; ;; ghc-mod setup (turn off to use haskell-mode's features, which are a bit heavier
+;; (autoload 'ghc-init "ghc" nil t)
+;; (autoload 'ghc-debug "ghc" nil t)
+;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
-;; popup info
-(require 'company)
-(add-hook 'haskell-mode-hook 'company-mode)
-(add-to-list 'company-backends 'company-ghc)
-(custom-set-variables '(company-ghc-show-info t))
+;; ;; popup info
+;; (require 'company)
+;; (add-hook 'haskell-mode-hook 'company-mode)
+;; (add-to-list 'company-backends 'company-ghc)
+;; (custom-set-variables '(company-ghc-show-info t))
