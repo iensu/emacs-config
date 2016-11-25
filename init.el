37fa7c899f41a15100b8fab339f92efd9d62670f
Jens Östlund <jostlund@gmail.com>
2016-11-25 08:10:38 +0100

    Add Haskell support

--- init.el
+++ init.el
@@ -298,6 +298,26 @@
 (use-package git-timemachine
   :ensure t)
 
+(use-package ghc
+  :ensure t
+  :config
+  (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
+    (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
+    (add-to-list 'exec-path my-cabal-path))
+  (custom-set-variables '(haskell-tags-on-save nil)))
+
+(use-package haskell-mode
+  :ensure t
+  :config
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil nil)
+  (add-hook 'haskell-mode-hook (lambda () (ghc-init))))
+
+(use-package hindent
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook #'hindent-mode))
+
 (use-package hydra
   :ensure t)
 
