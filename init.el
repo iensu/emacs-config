5df786ddccaa793a11228cc6ef408a1aa09fa961
Jens Östlund <jostlund@gmail.com>
2016-12-03 16:44:55 +0100

    Add rust support

--- init.el
+++ init.el
@@ -499,6 +499,39 @@
   (add-hook 'restclient-mode-hook 'electric-pair-mode)
   (add-hook 'restclient-mode-hook 'electric-indent-mode))
 
+(use-package rust-mode
+  :ensure t
+  :bind (:map rust-mode-map
+         ("C-c <tab>" . rust-format-buffer))
+  :config
+  (iensu/add-auto-mode 'rust-mode "\\.rs$"))
+
+(use-package cargo
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook #'cargo-minor-mode))
+
+(use-package flycheck-rust
+	     :ensure t
+	     :config
+	     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package racer
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'rust-mode-hook #'eldoc-mode)
+  (setq racer-cmd "~/.cargo/bin/racer"
+        racer-rust-src-path "/usr/local/src/rustc-1.8.0/src"))
+
+(use-package company-racer
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook (lambda ()
+                              (add-to-list 'company-backends 'company-racer)))
+  (setq company-tooltip-align-annotations t
+        company-racer-rust-src "/usr/local/src/rustc-1.8.0/src"))
+
 (use-package smart-mode-line
   :ensure t
   :init
