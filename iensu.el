aef4288b8ca12a2e3432a45287bff258d8c47808
Jens Östlund <jostlund@gmail.com>
2016-12-09 12:39:09 +0100

    Add move-file function

--- iensu.el
+++ iensu.el
@@ -112,5 +112,22 @@
    :contents-sources
    (list (cfw:org-create-source "Green"))))
 
+(defun iensu/move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one.  Copied from http://zck.me/emacs-move-file."
+  (interactive (list (if buffer-file-name
+                         (read-file-name "Move file to: ")
+                       (read-file-name "Move file to: "
+                                       default-directory
+                                       (expand-file-name (file-name-nondirectory (buffer-name))
+                                                         default-directory)))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (buffer-file-name)))
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+      (delete-file old-location))))
+
 (provide 'iensu)
 ;;; iensu.el ends here
