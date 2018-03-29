fe2d1fbb1c870c81a91e380e2529d82bf93be925
Jens Östlund <jostlund@gmail.com>
2018-03-29 22:53:08 +0200

    Use common lisp indentation mode for elisp

--- modules/lang/elisp.el
+++ modules/lang/elisp.el
@@ -7,7 +7,8 @@
 (defun iensu/-elisp-mode-hook ()
   (smartparens-mode -1)
   (paredit-mode 1)
-  (rainbow-delimiters-mode 1))
+  (rainbow-delimiters-mode 1)
+  (setq lisp-indent-function 'common-lisp-indent-function))
 
 (iensu-add-auto-mode 'emacs-lisp-mode "/Cask$")
 (add-hook 'emacs-lisp-mode-hook 'iensu/-elisp-mode-hook)
