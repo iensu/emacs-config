c070ebeed7e847fd1d3837d751d145dda74fb008
Jens Östlund <jostlund@gmail.com>
2018-03-30 10:46:21 +0200

    Remove common-elisp-indent-function

--- modules/lang/elisp.el
+++ modules/lang/elisp.el
@@ -7,8 +7,7 @@
 (defun iensu/-elisp-mode-hook ()
   (smartparens-mode -1)
   (paredit-mode 1)
-  (rainbow-delimiters-mode 1)
-  (setq lisp-indent-function 'common-lisp-indent-function))
+  (rainbow-delimiters-mode 1))
 
 (iensu-add-auto-mode 'emacs-lisp-mode "/Cask$")
 (add-hook 'emacs-lisp-mode-hook 'iensu/-elisp-mode-hook)
