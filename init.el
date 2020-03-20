e7db45d9811530049beef95b8bf51476a39d87a8
Jens Östlund <jostlund@gmail.com>
2020-03-20 21:55:49 +0100

    Fix wind-move in org files :D

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 (package-initialize)
 
+;; disable <shift>-<cursor> in org-mode
+;; needs to be set before org-mode is loaded
+(setq org-replace-disputed-keys t)
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "configuration.org"
