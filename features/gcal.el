dfe8fbb2c56a486e08f331bf0dcea54c38fde216
Jens Östlund <jostlund@gmail.com>
2020-11-27 15:29:00 +0100

    Fix calendar syncing

--- features/gcal.el
+++ features/gcal.el
@@ -3,11 +3,21 @@
   :init
   (setq org-gcal-token-file (expand-file-name ".local/org-gcal/org-gcal-token" user-emacs-directory)
         org-gcal-dir (expand-file-name ".local/org-gcal/" user-emacs-directory))
+
   :config
   (setq org-gcal-client-id iensu-gcal-client-id
         org-gcal-client-secret iensu-gcal-client-secret
-        org-gcal-file-alist `(("jens.ostlund@futurice.com" . ,(expand-file-name "calendars/work.org"
-                                                                                org-directory)))))
+
+        org-gcal-down-days 30
+        org-gcal-up-days 30
+        org-gcal-remove-api-cancelled-events t
+
+        ;; https://github.com/kidd/org-gcal.el/issues/107
+        org-id-locations-file (expand-file-name ".local/.org-id-locations" user-emacs-directory))
+
+  (let ((work-calendar (expand-file-name "calendars/work.org" org-directory)))
+    (setq org-gcal-file-alist `(("jens.ostlund@futurice.com" . ,work-calendar)))
+    (add-to-list 'org-agenda-files work-calendar)))
 
 (defun iensu/refresh-work-calendar ()
   "Fetch Google calendar events and add the proper file tag(s)."
