b8cb9c6c63159dd25aa5eb05037daeb4efa2874d
Jens Östlund <jostlund@gmail.com>
2021-12-21 00:11:30 +0100

    LilyPond-mode is weird...

--- features/lang-lilypond.el
+++ features/lang-lilypond.el
@@ -1,5 +1,7 @@
-(use-package lilypond
-  :mode
-  ("\\.ly$" . LilyPond-mode)
-  :init
-  (require 'lilypond-mode))
+(use-package lilypond)
+
+(add-to-list 'load-path (concat straight-base-dir "straight/repos/lilypond/elisp"))
+
+(autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
+(add-to-list 'auto-mode-alist '("\\.ly\\'" . LilyPond-mode))
+(add-to-list 'auto-mode-alist '("\\.ily\\'" . LilyPond-mode))
