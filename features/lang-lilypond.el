0cc77874191be83c8df4f6968b95693bd7056535
Jens Östlund <jostlund@gmail.com>
2021-12-21 22:34:30 +0100

    Fix lilypond-mode once and for all

--- features/lang-lilypond.el
+++ features/lang-lilypond.el
@@ -1,7 +1,5 @@
-(use-package lilypond)
-
-(add-to-list 'load-path (concat straight-base-dir "straight/repos/lilypond/elisp"))
-
+;; Can't get lilypond to load reliably with use-package and straight...
+(add-to-list 'load-path (expand-file-name "packages/lilypond" user-emacs-directory))
 (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
 (add-to-list 'auto-mode-alist '("\\.ly\\'" . LilyPond-mode))
 (add-to-list 'auto-mode-alist '("\\.ily\\'" . LilyPond-mode))
