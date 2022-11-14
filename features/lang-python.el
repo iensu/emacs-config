c4c400815ab859075a22437f4e7faa427d62e04e
Jens Östlund <jostlund@gmail.com>
2022-11-14 10:27:47 +0100

    Conditionally enable black in Python buffers

--- features/lang-python.el
+++ features/lang-python.el
@@ -1,1 +1,9 @@
 (add-hook 'python-mode-hook 'eglot-ensure)
+
+(use-package python-black
+  :demand t
+  :after python
+  :config
+  (add-hook 'python-mode-hook (lambda ()
+                                (when (executable-find "black")
+                                  (python-black-on-save-mode 1)))))
