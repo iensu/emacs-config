369137dce7b67a58b2136b9f2cc9d222a191ec12
Jens Östlund <jostlund@gmail.com>
2023-01-06 10:33:11 +0100

    Add shortcut to run Rust tests

--- features/lang-rust.el
+++ features/lang-rust.el
@@ -1,6 +1,7 @@
 (use-package rust-mode
   :bind (:map rust-mode-map
-              ("C-c C-c" . rust-run-clippy))
+              ("C-c C-c" . rust-run-clippy)
+              ("C-c C-t" . rust-test))
   :hook
   (rust-mode . eglot-ensure)
   :config
