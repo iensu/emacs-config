d2091c7e83f2da958167bb3da041f739e27ebecf
Jens Östlund <jostlund@gmail.com>
2023-02-24 14:35:18 +0100

    Add a convenience function to run Cargo Clippy with args

--- features/lang-rust.el
+++ features/lang-rust.el
@@ -1,8 +1,23 @@
 (use-package rust-mode
   :bind (:map rust-mode-map
-              ("C-c C-c" . rust-run-clippy)
+              ("C-c C-c" . iensu/rust-check-project)
               ("C-c C-t" . rust-test))
   :hook
   (rust-mode . eglot-ensure)
   :config
   (setq rust-format-on-save t))
+
+(defvar iensu-rust-check-project-clippy-params '()
+  "Params to send to the clippy command.")
+
+(defun iensu/rust-check-project ()
+  "Run either Clippy or standard `cargo check' to check the current project."
+  (interactive)
+  (let* ((root-dir (project-root (project-current t)))
+         (cargo-file (expand-file-name (concat root-dir "Cargo.toml"))))
+    (if (not (executable-find "cargo-clippy"))
+        (compile (format "cargo check --manifest-path=%s --workspace" cargo-file))
+      (let ((clippy-args (if iensu-rust-check-project-clippy-params
+                             (string-join (append '("--") iensu-rust-check-project-clippy-params) " ")
+                           "")))
+        (compile (format "cargo clippy --manifest-path=%s %s" cargo-file clippy-args))))))
