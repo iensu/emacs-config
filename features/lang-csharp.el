84b6a7eddb173103d4efcbc1afc60d84ab29f6a9
Jens Östlund <jostlund@gmail.com>
2023-08-10 23:30:47 +0200

    Change default indent offset in C# buffers

--- features/lang-csharp.el
+++ features/lang-csharp.el
@@ -4,7 +4,7 @@
   :config
   (defun iensu--csharp-mode-hook ()
     (c-set-offset 'arglist-intro '+)
-    (setq c-basic-offset 2))
+    (setq c-basic-offset 4))
   (add-hook 'csharp-mode-hook #'iensu--csharp-mode-hook))
 
 (add-to-list 'auto-mode-alist '("\\.csproj$" . xml-mode))
