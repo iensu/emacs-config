a3fbd3b4a0f12de37baf75e2759ffd6f51d66800
Jens Östlund <jostlund@gmail.com>
2023-08-20 22:54:16 +0200

    Simplify C# mode configuration

--- features/lang-csharp.el
+++ features/lang-csharp.el
@@ -1,11 +1,14 @@
-(use-package csharp-mode
-  :mode ("\\.cs$" "\\.cshtml")
-  :hook (csharp-mode . eglot-ensure)
-  :config
-  (defun iensu--csharp-mode-hook ()
-    (c-set-offset 'arglist-intro '+)
-    (setq c-basic-offset 4))
-  (add-hook 'csharp-mode-hook #'iensu--csharp-mode-hook))
+(require 'csharp-mode)
+
+(dolist (ext '("\\.cs$" "\\.cshtml"))
+  (add-to-list 'auto-mode-alist `(,ext . csharp-ts-mode)))
 
 (add-to-list 'auto-mode-alist '("\\.csproj$" . xml-mode))
 (add-to-list 'auto-mode-alist '("function.proj$" . xml-mode))
+
+(defun iensu--csharp-mode-hook ()
+  (eglot-ensure)
+  (c-set-offset 'arglist-intro '+)
+  (setq c-basic-offset 4))
+
+(add-hook 'csharp-ts-mode-hook #'iensu--csharp-mode-hook)
