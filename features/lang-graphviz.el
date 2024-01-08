0465ddef07570571d7a03db0dca2cafff49f1c6b
Jens Östlund <jostlund@gmail.com>
2024-01-08 10:21:23 +0100

    Update graphviz configuration

--- features/lang-graphviz.el
+++ features/lang-graphviz.el
@@ -1,5 +1,10 @@
 (use-package graphviz-dot-mode
   :bind (:map graphviz-dot-mode-map
               ("C-c C-c" . graphviz-dot-preview))
+  :hook
+  (graphviz-dot-mode . lsp-deferred)
+  :init
+  (require 'lsp-dot)
   :config
-  (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot)))
+  (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot))
+  (setopt graphviz-dot-preview-extension "svg"))
