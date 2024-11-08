0ac4a242c88c51c25786ef0424b44019324d8426
Jens Östlund <jostlund@gmail.com>
2024-11-08 08:33:40 +0100

    Read postgres password from auth-sources

--- features/lang-sql.el
+++ features/lang-sql.el
@@ -1,8 +1,41 @@
-(use-package sql-mode
-  :mode "\\.psql$"
-  :ensure nil
-  :config
-  (add-hook 'sql-mode-hook
-            (lambda ()
-              (when (string= (file-name-extension buffer-file-name) "psql")
-                (setopt sql-product 'postgres)))))
+(require 'sql)
+
+(add-to-list 'auto-mode-alist '("\\.psql\\'" . sql-mode))
+
+(add-hook 'sql-mode-hook
+          (lambda ()
+            (when (string= (file-name-extension buffer-file-name) "psql")
+              (setopt sql-product 'postgres))))
+
+;; Use `auth-sources' to look for passwords
+(setopt sql-password-wallet auth-sources)
+
+;; Make sql-postgres check for passwords
+(add-to-list 'sql-postgres-login-params 'password)
+
+(defun sql-comint-postgres (product options &optional buf-name)
+  "Create comint buffer and connect to Postgres.
+
+ Overrides the original function in order to be able to use the password
+ fetched from `auth-source-search'.
+
+ Originally the arguments are sent as options to `psql', however there is
+ no option to pass the password, so here I'm building up a connection URI
+ instead."
+  (let ((params
+         (append options
+                 (list (string-join (append
+                                     '("postgres://")
+                                     (if (not (string= "" sql-user))
+                                         (list sql-user))
+                                     (if sql-password
+                                         (list ":" (if (functionp sql-password) (funcall sql-password) sql-password)))
+                                     (if (or sql-password sql-user)
+                                         (list "@"))
+                                     (if (not (string= "" sql-server))
+                                         (list sql-server))
+                                     (if (not (= 0 sql-port))
+                                         (list ":" (number-to-string sql-port)))
+                                     (if (not (string= "" sql-database))
+                                         (list "/" sql-database))))))))
+    (sql-comint product params buf-name)))
