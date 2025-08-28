99eb8e126ea82d6ef6df23d22754ddd8186f7f9d
Jens Östlund <jostlund@gmail.com>
2025-08-28 20:10:42 +0200

    Add mode for prisma schema files

---
+++ features/lang-prisma.el
@@ -0,0 +1,4 @@
+(use-package prisma-mode
+  :vc (prisma-mode :url "https://github.com/pimeys/emacs-prisma-mode"
+                   :rev "f7744a995e84b8cf51265930ce18f6a6b26dade7")
+  :mode (("\\.prisma$" . prisma-mode)))
