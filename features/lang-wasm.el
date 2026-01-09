9fc948bf7b2e3ccad4beb1e7492e9e184abb1cc0
Jens Östlund <jostlund@gmail.com>
2026-01-09 09:44:05 +0100

    Add syntax highlighting for WebAssembly Interface Types (.wit files)

--- features/lang-wasm.el
+++ features/lang-wasm.el
@@ -1,4 +1,12 @@
+;; WebAssembly Text Format (WAT)
 (use-package wat-mode
   :vc (wat-mode :url "https://github.com/devonsparks/wat-mode")
   :config
   (add-to-list 'org-src-lang-modes '("wat" . wat)))
+
+;; WebAssembly Interface Types (WIT)
+(use-package wit-mode
+  :vc (wit-mode :url "https://git.sr.ht/~iensu/wit-mode")
+  :mode "\\.wit\\'"
+  :config
+  (add-to-list 'org-src-lang-modes '("wit" . wit)))
