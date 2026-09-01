97e70248b43f4f3db65ce10d8ccf08a9658da326
Jens Östlund <hi@iensu.com>
2026-09-01 11:27:35 +0200

    Add vc-use-package

--- init.el
+++ init.el
@@ -1185,7 +1185,8 @@ Falls back to looking for .projectile for compatibility reasons."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-vc-selected-packages
-   '()))
+   '((vc-use-package :vc-backend Git :url
+		     "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
